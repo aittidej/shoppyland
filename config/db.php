68d49f3e44db9478934113b6f5fcffc8461f533a
AJ Tan <ettidej@gmail.com>
2018-07-17 11:19:44 -0700

    + First add

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+	'dsn' => 'pgsql:host=127.0.0.1;port=5432;dbname=shoppyland',
+    'username' => 'dbuser',
+    'password' => 'dbuser220',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
