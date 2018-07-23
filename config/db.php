470f9279b05dbacea2ca065413cbedd015d880bb
AJ Tan <ettidej@gmail.com>
2018-07-23 09:29:57 -0700

    ~

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=shoppylandbyhoney.clvsgybmj76w.us-west-1.rds.amazonaws.com;port=3306;dbname=shoppyland',
-    'username' => 'aittidej',
-    'password' => 'ohm33298',
-    'charset' => 'utf8',
-];
